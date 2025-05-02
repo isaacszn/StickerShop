a3da98b1664a861f0279fa16715488c9df4da814
Isaac Szn <isaaczik9@gmail.com>
2025-05-02 10:08:57 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+// Purchase functionality
 document.querySelector('.purchase-btn').addEventListener('click', () => {
   document.querySelector('.whole-page').classList.add('blur')
   document.querySelector('.simple-dialog').classList.remove('d-none')
   document.querySelector('.simple-dialog').classList.add('d-block')
+})
+
+// Cancel functionality
+document.querySelector('.cancel-btn').addEventListener('click', () => {
+  document.querySelector('.whole-page').classList.remove('blur')
+  document.querySelector('.simple-dialog').classList.add('d-none')
+  document.querySelector('.simple-dialog').classList.remove('d-block')
+})
+
+// Show menu functionality
+document.querySelector('.menu-btn').addEventListener('click', () => {
+  document.querySelector('.whole-page').classList.add('blur')
+  document.querySelector('.menu').classList.remove('hide')
+  document.querySelector('.menu').classList.add('show')
+})
+
+// Close menu functionality
+document.querySelector('.close-btn').addEventListener('click', () => {
+  document.querySelector('.whole-page').classList.remove('blur')
+  document.querySelector('.menu').classList.add('hide')
+  document.querySelector('.menu').classList.remove('show')
 })
