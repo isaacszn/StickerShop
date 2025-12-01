10f8c5503d452840840b39e5b604a8f9b4ae1ed2
Zik Szn <isaaczik9@gmail.com>
2025-12-01 03:02:02 -0800

    Added a gitignore file and added Cocobase SDK

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,9 +1,12 @@
+import { Cocobase } from "https://unpkg.com/cocobase@1.2.1/dist/index.js";
+
 document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     
     const btn = document.querySelector("button");
     const originalContent = btn.textContent;
     btn.textContent = "Logging in..."
+    btn.classList.add("disabled")
     const formData = new FormData(e.target)
     const email = formData.get("email")
     const password = formData.get("password")
@@ -11,4 +14,5 @@ document.querySelector("form").addEventListener("submit", (e) => {
     console.log(`${email} ${password}`);
 
     // Implement Cocobase here to log in user
+    console.log(Cocobase)
 });
--- assets/scripts/signup.js
+++ assets/scripts/signup.js
@@ -1,9 +1,12 @@
+import { Cocobase } from "https://unpkg.com/cocobase@1.2.1/dist/index.js";
+
 document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     
     const btn = document.querySelector("button");
     const originalContent = btn.textContent;
     btn.textContent = "Signing up..."
+    btn.classList.add("disabled")
     const formData = new FormData(e.target)
     const fullName = formData.get("full-name")
     const email = formData.get("email")
@@ -12,4 +15,5 @@ document.querySelector("form").addEventListener("submit", (e) => {
     console.log(`${fullName} ${email} ${password}`);
 
     // Implement Cocobase here to create user account
+    console.log(Cocobase)
 });
