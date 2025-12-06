ec2d8a2e6db0b1a73c1799b7398c11912b2b02af
Zik Szn <isaaczik9@gmail.com>
2025-12-06 03:20:49 -0800

    Did some updates; added a new menu for mobile

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -30,18 +30,28 @@ document.querySelector(".continue-btn").addEventListener("click", () => {
   }
 });
 
-// Show menu functionality
+// Open menu functionality
 document.querySelector(".menu-btn").addEventListener("click", () => {
   document.querySelector(".whole-page").classList.add("blur");
-  document.querySelector(".menu").classList.remove("hide");
-  document.querySelector(".menu").classList.add("show");
+  const menu = document.querySelector(".menu");
+  const list = document.querySelector("#menu-list");
+  const closeBtn = document.querySelector("#close-button");
+  menu.style.width = "70vw";
+  menu.style.padding = "1rem";
+  list.classList.remove("d-none");
+  closeBtn.style.display = "flex";
 });
 
 // Close menu functionality
 document.querySelector(".close-btn").addEventListener("click", () => {
   document.querySelector(".whole-page").classList.remove("blur");
-  document.querySelector(".menu").classList.add("hide");
-  document.querySelector(".menu").classList.remove("show");
+  const menu = document.querySelector(".menu");
+  const list = document.querySelector("#menu-list");
+  const closeBtn = document.querySelector("#close-button");
+  menu.style.width = "0";
+  menu.style.padding = "0";
+  list.classList.add("d-none");
+  closeBtn.style.display = "none";
 });
 
 // PAYSTACK
